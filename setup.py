0fe1ae4ceddb5e45d0c889eecb33d895632b3d73
donmezk <donmezk@outlook.com>
2020-03-25 20:28:21 +0300

    installing library option

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Kutay
+"""
+
+from distutils.core import setup
+setup(name='visjobs',
+      version='1.0',
+      author='Kutay&Berkay DONMEZ',
+      packages=['visjobs'],
+)

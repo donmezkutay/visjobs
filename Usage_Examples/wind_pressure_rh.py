02e839deeb9809803c21d44d0e254f4e3426619d
donmezk <donmezk@outlook.com>
2020-03-25 19:59:28 +0300

    Updating map capabilities

--- Usage_Examples/wind_pressure_rh.py
+++ Usage_Examples/wind_pressure_rh.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[11]:
 
 
+#importing dependencies
 from visjobs.datas import get_data
-data = get_data.pick_data( hour='12',latest=True,model='GFS', hourly=False)
-
 from visjobs.visualize import draw_map
+import xarray as xr
 
-press = data['prmslmsl'][0:30:1,:,:].sel(lat = slice(30,65) ,lon = slice(0,48)) / 100
-ugrd =  data['ugrd10m'][0:30:1,:,:].sel(lat = slice(30,65) ,lon = slice(0,48)) * 1.94384449
-vgrd =  data['vgrd10m'][0:30:1,:,:].sel(lat = slice(30,65) ,lon = slice(0,48)) * 1.94384449
-rh =  data['rhprs'][0:30:1,:,:].sel(lat = slice(30,65) ,lon = slice(0,48), lev=700) 
-time = len(data['time'][0:30:1])
-
-lon_iso =press.lon[:]
-lat_iso = press.lat[:]
-
-
-# In[6]:
+#getting the data using pick_data function of visjobs.datas
+#hour=06 means the 06Z run of the model 
+#here latest=True means the latest output with 06Z run
+#model is chosen GFS can be changed to NAM also
+#if hourly=False the GFS model will be 3 hourly -->only valid for GFS not for NAM
+data = get_data.pick_data( hour='06',latest=True,model='GFS', hourly=False)
 
 
+#here using xarray dataset,  we are dedicating the interval of desired latitude and longitude
+# [0:2:1,:,:] means --> ['time', 'lon','lat']
+press = data['prmslmsl'][0:2:1,:,:].sel(lat = slice(30,65), lon=slice(0,48))  / 100
+ugrd =  data['ugrd10m'][0:2:1,:,:].sel(lat = slice(30,65), lon=slice(0,48) ) * 1.94384449
+vgrd =  data['vgrd10m'][0:2:1,:,:].sel(lat = slice(30,65), lon=slice(0,48) ) * 1.94384449
+rh =  data['rhprs'][0:2:1,:,:].sel(lat = slice(30,65), lon=slice(0,48) , lev=700) 
+time = len(data['time'][0:2:1])
+
+
+
+# In[15]:
+
+
+#choosing the desired plot size
+import numpy as np
 from pylab import rcParams
 rcParams['figure.figsize'] = 21,24
-draw_map.wind_pressure_rh(time, press, rh, ugrd, vgrd, lon_iso, lat_iso, extent=[0, 48, 30, 60], 
-                          save_where='Pictures\wind_pressure_rh{}.png', breaking=False )
+
+#place='europe' indicates the basin that user want to show as a plot.
+#places avaliable for plotting
+#places= np.array(['europe','northamerica','australia','gulfofmexico','carribeans','indianocean'])
+#if breaking=True only a single map will be created even if a bunch of time is introduced 
+#if title_on=False the title will be missed
+#if the title_on=True, title will be plotted and one can change it posisition arguments using
+#tl1,tl2,tl3 etc. parameters.
+# here tl5 is used to change the position of owner_name
+#owner_name plots the upper-left sign.
+draw_map.wind_pressure_rh(time, press, rh, ugrd, vgrd, place='europe',
+                          save_where='Pictures\wind_pressure_rh{}.png', breaking=True, title_on=True ,owner_name='Kutay DÖNMEZ',
+                         tl5=[0.0047, 0.98100])
 
 
 # In[ ]:
--- visjobs/visualize/draw_map.py
+++ visjobs/visualize/draw_map.py
@@ -18,12 +18,59 @@ import matplotlib.animation as animation
 def arrangements_of_extras(title1=[], title2=[], title3=[], title4=[], title5=[]):
     return title1, title2, title3, title4, title5
 
-def wind_pressure_rh(time_on, pressure, hum, u, v, lon_iso, lat_iso, extent=[], hourly=3, 
-                      save_where='wind_pressure_rh{}.png'
+def wind_pressure_rh(time_on, pressure, hum, u, v, place='europe',
+                     hourly=3, 
+                      save_where='wind_pressure_rh{}.png',title_on=False
+                      ,owner_name='Kutay&Berkay DONMEZ',
+     plot_main_title='GFS MSLP(hPa)|10m Wind(knots)|700hPa RH(%80 to %100 Green Contours)'
                      , breaking=True, tl1=[0,1.02], tl2=[0,1.0050], 
                      tl3=[0.5,1.0050], tl4=[0.81000,1.0050], tl5=[0.0047, 0.98422]):
+    """ Returns a pre-prepared windspeed-pressure-rh plot
+        
+    time_on=indicates the count of maps(hours) will be plotted
+    extent:
+        
+    #places avaliable for plotting
+    places= np.array(['europe','northamerica','australia','gulfofmexico','carribeans'])
     
-    #define starting time using datetime
+    If title_on=False the titles will not be seen, unless the title_on=True,
+    Default is False.
+    
+    NOTE:Please Input Xarray DataArray
+    """
+    
+    #warn user to only input xarray dataset as variables
+    try:
+        press = pressure.sel(time=pressure['time'][0] ) 
+    except:
+        raise('Please Input a proper Xarray DataArray')
+        
+    try:
+        rh = hum.sel(time=hum['time'][0] ) 
+    except:
+        raise('Please Input a proper Xarray DataArray')
+    
+    try: 
+        uwind = u.sel(time=u['time'][0] ) 
+    except:
+        raise('Please Input a proper Xarray DataArray') 
+    
+    try:vwind = v.sel(time=v['time'][0] ) 
+    except:
+        raise('Please Input a proper Xarray DataArray')
+    
+    #defining lon and lat
+    lon_iso = pressure.lon[:].values
+    lat_iso = pressure.lat[:].values
+    
+    #places avaliable for plotting
+    places= np.array(['europe','northamerica','australia','gulfofmexico','carribeans','indianocean'])
+    
+    #extents corresponding to the places defined
+    extents = np.array([[0, 48, 30, 60],[218,318,0,60],
+                        [80,180,-50,10],[260,285,18,31],[275,300,12,38],[30, 130,-25,25]])
+        
+    #define starting time
     start_time_all = datetime.now()
 
     #define our loop ingredients
@@ -53,7 +100,9 @@ def wind_pressure_rh(time_on, pressure, hum, u, v, lon_iso, lat_iso, extent=[], 
         ax.add_feature(cartopy.feature.LAND.with_scale('10m') ,facecolor='lightgrey')
         
         #defining extent area
-        ax.set_extent(extent)
+        for i in range(len(places)):
+            if place == places[i]:
+                ax.set_extent(extents[i])
         
         #define a colormap
         c1 = plt.cm.cool(np.linspace(0., 1, 256))
@@ -91,17 +140,17 @@ def wind_pressure_rh(time_on, pressure, hum, u, v, lon_iso, lat_iso, extent=[], 
         wind_slicelat = slice(5, -5, 7)
         wind_slicelon = slice(5, -5, 3)
         
-        
-        title1 = ax.text(tl1[0],tl1[1],'GFS MSLP(hPa)|10m Wind(knots)|700hPa RH(%80 to %100 Green Contours)',transform=ax.transAxes,fontsize=23, weight='bold',style='italic')
-        title2 = ax.text(tl2[0],tl2[1],'Init: {}'.format(str(pressure['time'][0].values)[0:13]),transform=ax.transAxes,fontsize=17,style='italic')
-        title3 = ax.text(tl3[0],tl3[1],'Hour: {}'.format(toplam),transform=ax.transAxes,fontsize=18,style='italic')
-        title4 = ax.text(tl4[0],tl4[1],'Valid: {}'.format(valid),transform=ax.transAxes,fontsize=18,weight='heavy',style='italic')
-        title5 = ax.text(tl5[0],tl5[1], "Kutay & Berkay DONMEZ",transform=ax.transAxes, size=18,zorder=17,style='italic',
-
-             bbox=dict(boxstyle="square",alpha=0.7,
-                       ec='black',
-                       fc='white',
-                       ))
+        if title_on==True:
+            title1 = ax.text(tl1[0],tl1[1],plot_main_title,transform=ax.transAxes,fontsize=23, weight='bold',style='italic')
+            title2 = ax.text(tl2[0],tl2[1],'Init: {}'.format(str(pressure['time'][0].values)[0:13]),transform=ax.transAxes,fontsize=17,style='italic')
+            title3 = ax.text(tl3[0],tl3[1],'Hour: {}'.format(toplam),transform=ax.transAxes,fontsize=18,style='italic')
+            title4 = ax.text(tl4[0],tl4[1],'Valid: {}'.format(valid),transform=ax.transAxes,fontsize=18,weight='heavy',style='italic')
+            title5 = ax.text(tl5[0],tl5[1], owner_name,transform=ax.transAxes, size=18,zorder=17,style='italic',
+    
+                 bbox=dict(boxstyle="square",alpha=0.7,
+                           ec='black',
+                           fc='white',
+                           ))
         
         #check if the data is hourly or 3 hourly or 12 hourly
         if hourly == 1:
@@ -135,15 +184,59 @@ def wind_pressure_rh(time_on, pressure, hum, u, v, lon_iso, lat_iso, extent=[], 
     print('wind_pressure_rh | TOTAL JOB DONE | Duration: {}'.format(end_time_all - start_time_all))
 
     
-def temp_rh_cross_aegean(time_on, temp, hum, height_iso, lat_iso, hourly=3, 
-                      save_where='temp_rh_cross{}.png'
-                     , breaking=True, tl1=[0,1.04], tl2=[0,1.0080], 
+def temp_rh_cross_aegean(time_on, temp, hum, 
+                         lon_ave=False, hourly=3, world_map=False, 
+                      save_where='temp_rh_cross{}.png', title_on=False
+                      ,owner_name='Kutay&Berkay DONMEZ',
+     plot_main_title=''
+                     , breaking=True,
+                     left=0.07, width =  0.65,
+                    bottom = 0.6,
+                    height = 0.280, 
+        tl1=[0,1.04], tl2=[0,1.0080], 
                      tl3=[0.5,1.0080], tl4=[0.77000,1.0080], tl5=[0.0050, 0.9700]):
-
+    """
+    Returns cross section of either averaged longitude with pre-indicated 2 point latitude or
+            cross section of either averaged latitude with pre-indicated 2 point longitude
+            using lon_ave=False or True option;
+                        if lon_ave=False returns lat averaged cross section.
+    
+    if world_map is true the second axis will not be plotted.
+    
+    Using left-width-bottom-height, if world_map is activated,  one can change its positional arguments
+    Using tl's, if the title_on=True,  one can change the positions of titles if they are not satisfied on default
+            
+    Impotant:Provide lon, lat and height all together
+    """
     #define starting time
     start_time_all = datetime.now()
     
+    #warn user to only input xarray dataset as variables
+    try:
+        temperature = temp.sel(time=temp['time'][0] ) 
+    except:
+        raise('Please Input a proper Xarray DataArray')
+        
+    try:
+        rhum = hum.sel(time=hum['time'][0] ) 
+    except:
+        raise('Please Input a proper Xarray DataArray')
+    #indicat lon and lat
+    lon_iso = temp.lon[:].values
+    lat_iso = temp.lat[:].values
+    height_iso = temp.lev[:].values
     
+    #copy the data just in case:
+    copy_data = temp.copy()
+    
+    if lon_ave == True:
+        temp = temp.mean(dim='lon')
+        hum = hum.mean(dim='lon')
+        titleis = 'Lon Averaged'
+    elif lon_ave == False:
+        temp = temp.mean(dim='lat')
+        hum = hum.mean(dim='lat')
+        titleis = 'Lat Averaged'
     #define our loop parameters
     toplam=0
     say=0
@@ -173,41 +266,68 @@ def temp_rh_cross_aegean(time_on, temp, hum, height_iso, lat_iso, hourly=3,
         #define our arrangement for plotting our variables
         tm = np.arange(-40 , 41 , 2)
         r = np.arange(0 , 101 , 5)
-
-        #contour our temperature
-        mesh_temp=ax.contour(lat_iso,height_iso, temperature ,tm, cmap=mymap ,
-                    linestyles='solid' , linewidths=0.8)
-        ax.clabel(mesh_temp, fontsize=16, inline=1, inline_spacing=7,fmt='%i', rightside_up=True, use_clabeltext=True )
-
-        #contour our humidity
-        mesh_rh=ax.contour(lat_iso,height_iso, rhum ,r, colors='green' ,
-                    linestyles='solid' , linewidths=0.4)
-        ax.clabel(mesh_rh, fontsize=12, inline=1, inline_spacing=7,fmt='%i', rightside_up=True, use_clabeltext=True  )
         
-        #mesh our humidity
-        mesh_hum=ax.contourf(lat_iso,height_iso,rhum ,r,cmap='Greens',extend='both', )
-                        
-        #label and set size of our axeses
-        plt.ylabel('Pressure(hPa)', fontsize=16,weight='heavy',style='italic')
-        plt.xlabel('Latitude(°N)', fontsize=16,weight='heavy',style='italic')
-        ax.tick_params(axis='y', labelsize=14)
-        ax.tick_params(axis='x', labelsize=14)
+        #if lon_ave true pass lat_iso to contours
+        if lon_ave == True:
+            #contour our temperature
+            mesh_temp=ax.contour(lat_iso,height_iso, temperature ,tm, cmap=mymap ,
+                        linestyles='solid' , linewidths=0.8)
+            ax.clabel(mesh_temp, fontsize=16, inline=1, inline_spacing=7,fmt='%i', rightside_up=True, use_clabeltext=True )
+    
+            #contour our humidity
+            mesh_rh=ax.contour(lat_iso,height_iso, rhum ,r, colors='green' ,
+                        linestyles='solid' , linewidths=0.4)
+            ax.clabel(mesh_rh, fontsize=12, inline=1, inline_spacing=7,fmt='%i', rightside_up=True, use_clabeltext=True  )
+            
+            #mesh our humidity
+            mesh_hum=ax.contourf(lat_iso,height_iso,rhum ,r,cmap='Greens',extend='both', )
+                            
+            #label and set size of our axeses
+            plt.ylabel('Pressure(hPa)', fontsize=16,weight='heavy',style='italic')
+            plt.xlabel('Latitude(°N)', fontsize=16,weight='heavy',style='italic')
+            ax.tick_params(axis='y', labelsize=14)
+            ax.tick_params(axis='x', labelsize=14)
+            
+            #invert y axis with pressure decreasing upward
+            ax.invert_yaxis()
         
-        #invert y axis with pressure decreasing upward
-        ax.invert_yaxis()
+        #if lon_ave false pass lon_iso to contours
+        if lon_ave == False:
+            #contour our temperature
+            mesh_temp=ax.contour(lon_iso,height_iso, temperature ,tm, cmap=mymap ,
+                        linestyles='solid' , linewidths=0.8)
+            ax.clabel(mesh_temp, fontsize=16, inline=1, inline_spacing=7,fmt='%i', rightside_up=True, use_clabeltext=True )
+    
+            #contour our humidity
+            mesh_rh=ax.contour(lon_iso,height_iso, rhum ,r, colors='green' ,
+                        linestyles='solid' , linewidths=0.4)
+            ax.clabel(mesh_rh, fontsize=12, inline=1, inline_spacing=7,fmt='%i', rightside_up=True, use_clabeltext=True  )
+            
+            #mesh our humidity
+            mesh_hum=ax.contourf(lon_iso,height_iso,rhum ,r,cmap='Greens',extend='both', )
+                            
+            #label and set size of our axeses
+            plt.ylabel('Pressure(hPa)', fontsize=16,weight='heavy',style='italic')
+            plt.xlabel('Longitude(°E)', fontsize=16,weight='heavy',style='italic')
+            ax.tick_params(axis='y', labelsize=14)
+            ax.tick_params(axis='x', labelsize=14)
+            
+            #invert y axis with pressure decreasing upward
+            ax.invert_yaxis()
         
         #arrange the titles of map
-        title = ax.text(tl1[0],tl1[1],'GFS 36°N-40°N | mean(26.5°E-27.5°E) Aegean Reg.| Height-Temp(°C)-RH(%)',color='navy',transform=ax.transAxes,fontsize=17, weight='bold',style='italic')
-        title2 = ax.text(tl2[0],tl2[1],'Init: {}'.format(str(temp['time'][0].values)[0:13]),transform=ax.transAxes,fontsize=14,style='italic')
-        title3 = ax.text(tl3[0],tl3[1],'Hour: {}'.format(toplam),transform=ax.transAxes,fontsize=15,style='italic')
-        title4 = ax.text(tl4[0],tl4[1],'Valid: {}'.format(valid),transform=ax.transAxes,fontsize=15,weight='heavy',style='italic')
-        title5 = ax.text(tl5[0],tl5[1], "Kutay & Berkay DONMEZ",transform=ax.transAxes, size=15,zorder=17,style='italic',
-
-
-             bbox=dict(boxstyle="square",alpha=0.7,
-                       ec='black',
-                       fc='white',
-                       ))
+        if title_on == True:
+            title = ax.text(tl1[0],tl1[1], titleis+' '+plot_main_title, color='navy',transform=ax.transAxes,fontsize=17, weight='bold',style='italic')
+            title2 = ax.text(tl2[0],tl2[1],'Init: {}'.format(str(temp['time'][0].values)[0:13]),transform=ax.transAxes,fontsize=14,style='italic')
+            title3 = ax.text(tl3[0],tl3[1],'Hour: {}'.format(toplam),transform=ax.transAxes,fontsize=15,style='italic')
+            title4 = ax.text(tl4[0],tl4[1],'Valid: {}'.format(valid),transform=ax.transAxes,fontsize=15,weight='heavy',style='italic')
+            title5 = ax.text(tl5[0],tl5[1], owner_name, transform=ax.transAxes, size=15,zorder=17,style='italic',
+    
+    
+                 bbox=dict(boxstyle="square",alpha=0.7,
+                           ec='black',
+                           fc='white',
+                           ))
                         
         #set with which interval the hour will change 
         if hourly == 1:
@@ -219,35 +339,49 @@ def temp_rh_cross_aegean(time_on, temp, hum, height_iso, lat_iso, hourly=3,
 
         #arrange our second axisis placement showing where the cross section is implied
         
-        left, width = 0.07, 0.65
-        bottom = 0.6
-        height = 0.280
+        
         left_h = left+width+0.0305
         rect_box = [left_h, bottom, 0.21, height]
         
+        #if only world_map=True make second axis if not do not..
         #set second axes
-        ax2 = plt.axes(rect_box,projection=cartopy.crs.Mercator())
-                        
-        #provide sets to not seen
-        ax2.get_yaxis().set_ticks([])
-        ax2.get_xaxis().set_ticks([])
-
-        #define map using cartopy for second axes 
-        ax2.add_feature(cartopy.feature.BORDERS.with_scale('10m') , linewidths = 0.5)
-        ax2.add_feature(cartopy.feature.COASTLINE.with_scale('10m') , linewidths=0.9)
-        ax2.add_feature(cartopy.feature.LAND.with_scale('10m') ,zorder=0,facecolor='brown')
-        ax2.set_extent([25,29,35,42])
-        
-        #show the cross section area in the second axis map
-        ax2.plot([26.5,26.5],[36,40], c='k',transform = cartopy.crs.PlateCarree())
-        ax2.plot([27.5,27.5],[36,40], c='k',transform = cartopy.crs.PlateCarree())
-        ax2.plot([26.5,27.5],[36,36], c='k',transform = cartopy.crs.PlateCarree())
-        ax2.plot([26.5,27.5],[40,40], c='k',transform = cartopy.crs.PlateCarree())
-        
-        ax2.scatter([26.5,26.5],[36,40], c='k',transform = cartopy.crs.PlateCarree())
-        ax2.scatter([27.5,27.5],[36,40], c='k',transform = cartopy.crs.PlateCarree())
-        ax2.scatter([26.5,27.5],[36,36], c='k',transform = cartopy.crs.PlateCarree())
-        ax2.scatter([26.5,27.5],[40,40], c='k',transform = cartopy.crs.PlateCarree())
+        if world_map == True:
+            
+            ax2 = plt.axes(rect_box,projection=cartopy.crs.Mercator())
+                            
+            #provide sets to not seen
+            ax2.get_yaxis().set_ticks([])
+            ax2.get_xaxis().set_ticks([])
+            
+            copy_east = copy_data['lon'].values[0]
+            copy_west = copy_data['lon'].values[-1]
+            
+            copy_south = copy_data['lat'].values[0]
+            copy_north = copy_data['lat'].values[-1]
+            #define map using cartopy for second axes 
+            ax2.add_feature(cartopy.feature.BORDERS.with_scale('10m') , linewidths = 0.5)
+            ax2.add_feature(cartopy.feature.COASTLINE.with_scale('10m') , linewidths=0.9)
+            ax2.add_feature(cartopy.feature.LAND.with_scale('10m') ,zorder=0,facecolor='brown')
+            
+            #checking if longitude is between 180 and 360 will change it to negative west values.
+            if copy_east>180 or copy_west>180:
+                copy_east+=-360
+                copy_west+=-360
+            
+            
+            ax2.set_extent([copy_east-1.5,copy_west+1.5,
+                                copy_south-1.5,copy_north+1.5])
+            
+            #show the cross section area in the second axis map
+            ax2.plot([copy_east,copy_east],[copy_south,copy_north], c='k',transform = cartopy.crs.PlateCarree())
+            ax2.plot([copy_west,copy_west],[copy_south,copy_north], c='k',transform = cartopy.crs.PlateCarree())
+            ax2.plot([copy_east,copy_west],[copy_south,copy_south], c='k',transform = cartopy.crs.PlateCarree())
+            ax2.plot([copy_east,copy_west],[copy_north,copy_north], c='k',transform = cartopy.crs.PlateCarree())
+            
+            ax2.scatter([copy_east,copy_east],[copy_south,copy_north], c='k',transform = cartopy.crs.PlateCarree())
+            ax2.scatter([copy_west,copy_west],[copy_south,copy_north], c='k',transform = cartopy.crs.PlateCarree())
+            ax2.scatter([copy_east,copy_west],[copy_south,copy_south], c='k',transform = cartopy.crs.PlateCarree())
+            ax2.scatter([copy_east,copy_west],[copy_north,copy_north], c='k',transform = cartopy.crs.PlateCarree())
 
 
         #save our figure

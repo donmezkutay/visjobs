229945e545d7915908aece51ce72fc1adb682029
donmezkutay <donmezk@outlook.com>
2020-08-21 22:21:30 +0300

    Changes for pip package

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-# -*- coding: utf-8 -*-
-#under construction
-"""
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-@author: Kutay
-"""
-
-from distutils.core import setup
-setup(name='visjobs',
-      version='1.0',
-      author='Kutay&Berkay DONMEZ',
-      packages=['visjobs'],
+setuptools.setup(
+    name="visjobs", # Replace with your own username
+    version="0.0.1",
+    author="Kutay Donmez",
+    author_email="donmezk@outlook.com",
+    description="Get Latest Atmospheric Model Data | Analyse | Visualize Easily",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/donmezkutay/visjobs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
